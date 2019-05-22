388b462c1950779bd0af7d32a791a35ddb1b3316
Ohad Rau <ohadrau@google.com>
2019-05-22 10:42:43 -0700

    Remove deadcode

--- src/binder.js
+++ src/binder.js
@@ -39,7 +39,7 @@ function genBindings(configFile, wasmFile) {
     env.jsBuffer += `
 function ${functionName}(${params.join(',')}) {
   return ${wrapReturn}(
-    ${functionName}(
+    __wasm_exports.${functionName}(
       ${args.join(',')}
     )
   )
@@ -54,84 +54,3 @@ const env = genBindings('./test/basic-config.json', 'library.wasm')
 console.log(env.jsBuffer)
 console.log('========================================================')
 console.log(env.cBuffer)
-
-/*
-let env = createEnv('test.wasm')
-
-let struct_ptr_type = {
-  type: 'pointer',
-  params: [
-    { type: 'struct',
-      params: {
-	'a': { type: 'u32', params: [] },
-	'b': { type: 'pointer', params: [ { type: 'char', params: [] } ] }
-      }
-    }
-  ]
-}
-
-console.log("Generating convertor for:")
-console.log("\t" + type2ctype(struct_ptr_type))
-console.log(c2js(env, struct_ptr_type))
-
-let my_c_type = {
-  type: 'functionPointer',
-  params: [
-    [
-      { type: 'u64', params: [] },
-      { type: 'pointer', params: [ { type: 'i32', params: [] } ] },
-      { type: 'pointer', params: [ { type: 'char', params: [] } ] },
-      { type: 'array', params: [ { type: 'i32', params: [] }, 3] }
-    ],
-    { type: 'void', params: [] }
-  ]
-}
-
-console.log("Generating convertor for:")
-console.log("\t" + type2ctype(my_c_type))
-console.log(c2js(env, my_c_type))
-
-console.log("Generating sizeof:")
-console.log(wrapSizeof(env, my_c_type))
-
-console.log("Generating i64:")
-console.log(wrapI64Fn(env, 'f', [{type:'i64', params: []}], my_c_type))
-
-let my_stupid_type = {
-  type: 'array',
-  params: [
-    {
-      type: 'functionPointer',
-      params: [
-        [
-          {
-            type: 'pointer',
-            params: [
-              { type: 'array', params: [ { type: 'i32', params: [] }, 3 ] }
-            ]
-          }
-        ],
-        {
-          type: 'array',
-          params: [
-            { type: 'i32', params: [] },
-            3
-          ]
-        }
-      ]
-    },
-    3
-  ]
-}
-
-console.log("Generating concrete declarator:")
-console.log("\t" + type2cdecl(my_stupid_type, 'f'))
-
-console.log("Caching wrapper:")
-let [wrapperId, wrapperBody] = cacheWrapper(env, [{type: 'i32', params: []}], my_c_type)
-console.log("\tWrapper ID: " +  wrapperId)
-console.log("\tWrapper Body: " + wrapperBody)
-
-console.log(env.jsBuffer)
-console.log(env.cBuffer)
-*/
